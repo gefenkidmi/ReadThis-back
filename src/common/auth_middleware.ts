25ee8565d971a1b0c29db77129d44609a5ee2ae2
zivklein21 <zivklein@Mac.lan>
2025-01-19 21:14:15 +0200

    fix fetching posts, and likes

--- src/common/auth_middleware.ts
+++ src/common/auth_middleware.ts
@@ -1,41 +1,35 @@
-import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
 
 export interface AuthRequest extends Request {
   user?: { _id: string };
 }
-
-/*
-type Payload = {
-  _id: string;
-};
-*/
 
 const authMiddleware = (
   req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
-  const authorization = req.header("authorization");
+  const authorization = req.header("Authorization");
+  if (!authorization) {
+    res.status(401).json({ message: "Unauthorized: No token provided" });
+    return;
+  }
 
-  const token = authorization && authorization.split(" ")[1];
+  const token = authorization.split(" ")[1]; // חילוץ ה-token
   if (!token) {
-    res.status(401).send("Access Denied");
+    res.status(401).json({ message: "Unauthorized: Invalid token" });
     return;
   }
 
-  if (!process.env.TOKEN_SECRET) {
-    res.status(500).send("Server Error");
-    return;
-  }
-
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.TOKEN_SECRET!, (err, user) => {
     if (err) {
-      res.status(401).send("Access Denied");
+      res
+        .status(401)
+        .json({ message: "Unauthorized: Token expired or invalid" });
       return;
     }
-
-    req.user = user as { _id: string };
+    req.user = user as { _id: string }; // הוספת המשתמש ל-request
     next();
   });
 };
