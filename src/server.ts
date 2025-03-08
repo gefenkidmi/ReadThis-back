b0d2ab1219553ad56d9861b7e86ccdc4d45203f8
gefenkidmi <gefenkidmi@gmail.com>
2025-03-08 15:14:04 +0200

    fix image display

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,7 @@ app.use(
 app.use("/posts", postsRoute);
 app.use("/comments", commentsRoute);
 app.use("/auth", authRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // 4) Connect to MongoDB
 const db = mongoose.connection;
